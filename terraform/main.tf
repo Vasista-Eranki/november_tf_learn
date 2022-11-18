84e02ccd0d181f21ee1997bda6778a76496c8f0a
Vasista-Eranki <60959482+Vasista-Eranki@users.noreply.github.com>
2022-11-18 18:53:25 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.31.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscriptionid
+  tenant_id       = var.tenantid
+  client_id       = var.clientid
+  client_secret   = var.clientsecret
+}
+
+locals {
+  rg_tag = "${terraform.workspace}-rg"
+}
+
+resource "azurerm_resource_group" "hexa-day5-rg" {
+  location = "eastus"
+  name = "${local.rg_tag}-rg"
+  tags = { "Name" = local.rg_tag }
+}
